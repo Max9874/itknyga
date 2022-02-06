9c28245d078f41be8c6b95ffd21bbc6f45684aa8
Maxim Vyazovsky <vyazovskymaxim7777@gmail.com>
2022-02-06 18:23:11 +0300

    27 урок закончен.

--- lesson27(practika).py
+++ lesson27(practika).py
@@ -2,32 +2,35 @@ from tkinter import *
 
 main = Tk()
 main.title('Шифр Цезаря')
+main.geometry('350x320')
 
 
 def encrypt_button_click():
     encrypted_message = ""
-    message = message_text_entry.get()
+    message = message_entry_text.get(1.0, END)
     key = int(key_entry.get())
 
-    encrypted_text_entry.delete(0, END)
+    encrypted_text.delete(1.0, END)
+    message = message.replace('\n', '')
 
     for index in message:
         kod = ord(index) + key
         encrypted_message += chr(kod)
-    encrypted_text_entry.insert(0, encrypted_message)
+    encrypted_text.insert(1.0, encrypted_message)
 
 
 def decipher_button_click():
     decipher_message = ""
-    message = encrypted_text_entry.get()
+    message = encrypted_text.get(1.0, END)
     key = int(key_entry.get())
 
-    message_text_entry.delete(0, END)
+    message_entry_text.delete(1.0, END)
+    message = message.replace('\n', '')
 
     for index in message:
         kod = ord(index) - key
         decipher_message += chr(kod)
-    message_text_entry.insert(0, decipher_message)
+    message_entry_text.insert(1.0, decipher_message)
 
 
 canvas = Canvas(main, width=300, height=210)
@@ -35,23 +38,23 @@ canvas = Canvas(main, width=300, height=210)
 message_text_label = Label(text='Текст Сообщение')
 message_text_label.place(x=20, y=20)
 
-message_text_entry = Entry()
-message_text_entry.place(x=20, y=50, width=250)
+message_entry_text = Text(width=15, height=10)
+message_entry_text.place(x=20, y=50)
 
 key_label = Label(text='key')
-key_label.place(x=20, y=80)
+key_label.place(x=20, y=230)
 
 key_entry = Entry()
-key_entry.place(x=70, y=80, width=50)
+key_entry.place(x=70, y=230, width=70)
 
 encrypt_button = Button(text='Шифрувати', command=encrypt_button_click)
-encrypt_button.place(x=20, y=110, width=100)
+encrypt_button.place(x=20, y=260, width=120)
 
 decipher_button = Button(text='Розшифрувати', command=decipher_button_click)
-decipher_button.place(x=140, y=110, width=100)
+decipher_button.place(x=170, y=260, width=120)
 
 encrypted_text_label = Label(text='Зашифроване повiдомлення')
-encrypted_text_label.place(x=20, y=140)
+encrypted_text_label.place(x=170, y=20)
 
-encrypted_text_entry = Entry()
-encrypted_text_entry.place(x=20, y=170, width=250)
+encrypted_text = Text(width=15, height=10)
+encrypted_text.place(x=170, y=50)
